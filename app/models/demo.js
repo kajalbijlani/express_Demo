fac2ec555f2704bb90719996ab5c73730ff14ad0
kajalbijlani <43329045+kajalbijlani@users.noreply.github.com>
2019-11-28 16:47:30 +0530

    Create demo.js

---
+++ app/models/demo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const DemoSchema = mongoose.Schema(
+  {
+    title: String,
+    content: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Demo', DemoSchema);
